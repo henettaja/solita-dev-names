14e210f3db754aa6b8cf570fbaa88aebb280ebf2
henettaja <henri.vaisane@gmail.com>
2020-12-10 20:41:06 +0200

    clarified the solutions to the requirements on the main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import { Paper } from "@material-ui/core";
 import MaterialTable from "material-table";
 
 function App() {
@@ -28,7 +29,17 @@ function App() {
     return (
         <React.Fragment>
             <MaterialTable columns={columns}
-                           data={data}/>
+                           data={data}
+                           title="Solita Dev Academy 2021 exercise"
+            />
+            <Paper elevation={10}>
+                <div class="textcontainer">
+                    <p>1. List names and amounts, order by amount, most popular first - click on the column title "amount" on top of the table</p>
+                    <p>2. List names in alphabetical order - click on the column title "names" on top of the table</p>
+                    <p>3. Return the total amount of all the names - shown in the lower right hand corner of the table</p>
+                    <p>4. Return the amount of the name given as a parameter - search field in the upper right hand corner of the table</p>
+                </div>
+            </Paper>
         </React.Fragment>
     );
 }
